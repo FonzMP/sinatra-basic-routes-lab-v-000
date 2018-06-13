0342927db7db754de337177e1b9bc7e2b18cea17
Alex Wilson <fonzmp@gmail.com>
2018-06-13 16:00:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,4 +10,12 @@ class App < Sinatra::Base
     "My name is Alex"
   end
 
+  get '/hometown' do
+    "My hometown is Marietta, OH"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Exit Strategy"
+  end
+
 end
